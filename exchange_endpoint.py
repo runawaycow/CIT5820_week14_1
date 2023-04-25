19d5afbfa737d48936a05b538a93579998154d59
Dan Luo <luo_dana@yahoo.com>
2023-04-25 12:42:15 +1000

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -253,9 +253,11 @@ def address():
         
         if content['platform'] == "Ethereum":
             #Your code here
+            eth_sk, eth_pk = get_eth_keys()
             return jsonify( eth_pk )
         if content['platform'] == "Algorand":
             #Your code here
+            algo_sk, algo_pk = get_algo_keys()
             return jsonify( algo_pk )
 
 @app.route('/trade', methods=['POST'])
