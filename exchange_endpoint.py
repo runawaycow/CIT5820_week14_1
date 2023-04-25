8f73e6669e8ba61354245cea5b43f8967c59bb8a
Dan Luo <luo_dana@yahoo.com>
2023-04-25 12:41:06 +1000

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -90,13 +90,25 @@ def log_message(message_dict):
     msg = json.dumps(message_dict)
 
     # TODO: Add message to the Log table
-    
+    # Add message to the Log table
+    log = Log(message=json.dumps(message_dict))
+    g.session.add(log)
+    g.session.commit()
     return
 
 def get_algo_keys():
     
     # TODO: Generate or read (using the mnemonic secret) 
     # the algorand public/private keys
+    try:
+        with open("algo_keys.json", "r") as f:
+            keys = json.load(f)
+            algo_sk = keys['private_key']
+            algo_pk = keys['public_key']
+    except:
+        algo_sk, algo_pk = algosdk.account.generate_account()
+        with open("algo_keys.json", "w") as f:
+            json.dump({'private_key': algo_sk, 'public_key': algo_pk}, f)
     
     return algo_sk, algo_pk
 
@@ -106,16 +118,80 @@ def get_eth_keys(filename = "eth_mnemonic.txt"):
     
     # TODO: Generate or read (using the mnemonic secret) 
     # the ethereum public/private keys
-
+    try:
+        with open("eth_keys.json", "r") as f:
+            keys = json.load(f)
+            eth_sk = keys['private_key']
+            eth_pk = keys['public_key']
+    except:
+        with open(filename, "r") as f:
+            mnemonic = f.read()
+        account = eth_account.Account.from_mnemonic(mnemonic.strip())
+        eth_sk = account.key.hex()
+        eth_pk = Web3.toChecksumAddress(account.address)
+        with open("eth_keys.json", "w") as f:
+            json.dump({'private_key': eth_sk, 'public_key': eth_pk}, f)
+    
     return eth_sk, eth_pk
   
 def fill_order(order, txes=[]):
-    # TODO: 
-    # Match orders (same as Exchange Server II)
-    # Validate the order has a payment to back it (make sure the counterparty also made a payment)
-    # Make sure that you end up executing all resulting transactions!
-    
-    pass
+    # Get the opposite side of the order
+    #Your code here
+    #if all(key in order for key in ['sender_pk','buy_amount','sell_amount', 'receiver_pk', 'buy_currency', 'sell_currency']):
+        order_obj = order        
+        #session.add(order_obj)
+        #session.commit()
+        print('Here!!!')
+        for existing_order in g.session.query(Order).all():
+            if order_obj.sell_amount * existing_order.sell_amount >= order_obj.buy_amount * existing_order.buy_amount and existing_order.buy_currency == order_obj.sell_currency and existing_order.sell_currency == order_obj.buy_currency and existing_order.filled == None:
+                order_obj.filled = datetime.now()
+                existing_order.filled = datetime.now()
+                order_obj.counterparty_id = existing_order.id
+                existing_order.counterparty_id = order_obj.id
+                if order_obj.buy_amount>existing_order.sell_amount:
+                    order_r = {}
+                    order_r['filled'] = None
+                    order_r['creator_id'] = order_obj.id
+                    order_r['sender_pk'] = order_obj.sender_pk
+                    order_r['receiver_pk'] = order_obj.receiver_pk
+                    order_r['buy_currency'] = order_obj.buy_currency
+                    order_r['sell_currency'] = order_obj.sell_currency
+                    order_r['buy_amount'] = order_obj.buy_amount-existing_order.sell_amount
+                    order_r['sell_amount'] = order_r['buy_amount'] * order_obj.sell_amount/order_obj.buy_amount
+                    process_order(order_r)
+                    order_r_obj = Order(**{f:order_r[f] for f in fields})
+                    session.add(order_r_obj)
+                    session.commit()
+                    print('1:Child order added - 1')
+
+                elif existing_order.buy_amount>order_obj.sell_amount:
+                    order_r = {}
+                    order_r['filled'] = None
+                    order_r['creator_id'] = existing_order.id
+                    order_r['sender_pk'] = existing_order.sender_pk
+                    order_r['receiver_pk'] = existing_order.receiver_pk
+                    order_r['buy_currency'] = existing_order.buy_currency
+                    order_r['sell_currency'] = existing_order.sell_currency
+                    order_r['buy_amount'] = existing_order.buy_amount-order_obj.sell_amount
+                    order_r['sell_amount'] = order_r['buy_amount'] *existing_order.sell_amount/existing_order.buy_amount
+                    #process_order(order_r)
+                    order_r_obj = Order(**{f:order_r[f] for f in fields})
+                    session.add(order_r_obj)
+                    session.commit()
+                    print('2:Child order added - 2')
+                else:
+                    print('5: Child order NOT created - 5')
+                    #print(existing_order.buy_amount)
+                    #print(order_obj.sell_amount)
+                    #print(order_obj.buy_amount)
+                    #print(existing_order.sell_amount)
+                    #print('##########')
+
+                break
+            txes.append(order_obj)
+            txes.append(existing_order)
+            break
+        return txes
   
 def execute_txes(txes):
     if txes is None:
@@ -139,7 +215,28 @@ def execute_txes(txes):
     #          We've provided the send_tokens_algo and send_tokens_eth skeleton methods in send_tokens.py
     #       2. Add all transactions to the TX table
 
-    pass
+    for tx in algo_txes:
+        if send_tokens_algo(tx['receiver_pk'], algo_pk, algo_sk, tx['amount']):
+            print(f"Algo transaction {tx['order_id']} successful")
+            tx_row = TX(id=tx['order_id'], tx_hash="", platform="Algorand")
+            g.session.add(tx_row)
+        else:
+            print(f"Algo transaction {tx['order_id']} failed")
+            g.session.rollback()
+            return False
+
+    for tx in eth_txes:
+        if send_tokens_eth(tx['receiver_pk'], eth_pk, eth_sk, tx['amount']):
+            print(f"Ethereum transaction {tx['order_id']} successful")
+            tx_row = TX(id=tx['order_id'], tx_hash="", platform="Ethereum")
+            g.session.add(tx_row)
+        else:
+            print(f"Ethereum transaction {tx['order_id']} failed")
+            g.session.rollback()
+            return False
+
+    g.session.commit()
+    return True
 
 """ End of Helper methods"""
   
